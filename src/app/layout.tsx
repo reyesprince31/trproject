40742bd2f066000e80128c785354cda47c3f3cbf
Prince Reyes <77110899+reyesprince31@users.noreply.github.com>
2024-05-06 15:35:29 +0800

    Final Commit for changing nav name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ function TopNav() {
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
       <div>Gallery</div>
-      <div>Sign in</div>
+      <div>Log In</div>
     </nav>
   );
 }
