d2a45a576768f1cd638a4a1cb709b81add34c6aa
Prince Reyes <77110899+reyesprince31@users.noreply.github.com>
2024-05-08 16:05:43 +0800

    Update the file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ function TopNav() {
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
       <div>Galleria</div>
-      <div>Log In</div>
+      <div>Log In / Sign Up</div>
     </nav>
   );
 }
