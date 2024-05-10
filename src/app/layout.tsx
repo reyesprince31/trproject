d79b2bf45136e2bd94ed262c30f71ae881a7a520
Prince Reyes <77110899+reyesprince31@users.noreply.github.com>
2024-05-10 12:04:10 +0800

    Dev (#25)
    * rollback nav name to signin
    
    * Final Commit for changing nav name
    
    * refactor
    
    * change nav title
    
    * test TopNav
    
    * fix layout (#24)
    
    * change nav title (#10)
    
    * rollback nav name to signin
    
    * Final Commit for changing nav name
    
    * refactor
    
    * change nav title
    
    * test TopNav
    
    * fix layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ function TopNav() {
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
       <div>Galleria</div>
-      <div>Log In Test</div>
+      <div>Log In / Sign Up</div>
     </nav>
   );
 }
@@ -30,6 +30,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`font-sans ${inter.variable} flex flex-col gap-4`}>
+        <TopNav />
+        <TopNav />
         <TopNav />
         {children}
       </body>
